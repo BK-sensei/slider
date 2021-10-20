3b6c475f9c235810832d24fadd0cca6c4e5e87d6
becca <rebecca.kaci@gmail.com>
2021-10-20 16:45:59 +0200

    Slider entre les images grâce aux flêches de droite et de gauche réussi

--- index.js
+++ index.js
@@ -3,11 +3,39 @@ $(function(){
     // Slider entre les images grâce aux points
     $(".point").click(function() {
         var slideLeft = $(this).index() * 100
-        console.log(slideLef)
+        console.log(slideLeft)
 
             $(".slider").animate({
                 left: `-${slideLeft}%`
-            }, 3000)
+            }, 1000)
     })
 
+    // Slider entre les images grâce à la flêche droite
+    var numArrow = 0
+    $(".right-arrow").click(function() {
+        
+        numArrow = numArrow + 1
+        
+        if (numArrow > 4){
+            numArrow = 4
+        }
+
+            $(".slider").animate({
+                left: `-${numArrow * 100}%`
+            }, 1000)
+    })
+
+    // Slider entre les images grâce à la flêche gauche
+    $(".left-arrow").click(function() {
+
+        numArrow = numArrow - 1
+
+        if (numArrow < 0){
+            numArrow = 0
+        }
+
+        $(".slider").animate({
+                left: `-${numArrow *100}%`
+        }, 1000)
+    })
 })
