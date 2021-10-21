d1c343d1aa089727839fa8f294c20d1b590902d8
becca <rebecca.kaci@gmail.com>
2021-10-21 10:36:35 +0200

    Modifications du setInterval et rédaction du random picture en jquery

--- index.js
+++ index.js
@@ -1,41 +1,48 @@
 $(function(){
 
+    var indexGlobal = 0
 
     // Slider entre les images grâce aux points
     $(".point").click(function() {
-        var slideLeft = $(this).index() * 100
+        indexGlobal = $(this).index()
+        var slideLeft = indexGlobal * 100
         console.log(slideLeft)
 
             $(".slider").animate({
                 left: `-${slideLeft}%`
             }, 1000)
+
+            clearInterval(interval)
+            interval = setInterval(autoslide, 2000)
     })
 
     // Slider entre les images grâce à la flêche droite
-    var numArrow = 0
     $(".right-arrow").click(function() {
         
-        numArrow = numArrow + 1
+        indexGlobal = indexGlobal + 1
         
         // Exercice : être bloquer sur la dernière slide quand on clique sur la flêche de droite
-        // if (numArrow > 4){
-        //     numArrow = 4
+        // if (indexGlobal > 4){
+        //     indexGlobal = 4
         // }
 
         // Bonus 1 : Infinite slider
-        if (numArrow > 4){
-            numArrow = 0
+        if (indexGlobal > 4){
+            indexGlobal = 0
         }
 
             $(".slider").animate({
-                left: `-${numArrow * 100}%`
+                left: `-${indexGlobal * 100}%`
             }, 1000)
+
+            clearInterval(interval)
+            interval = setInterval(autoslide, 2000)
     })
 
     // Slider entre les images grâce à la flêche gauche
     $(".left-arrow").click(function() {
 
-        numArrow = numArrow - 1
+        indexGlobal = indexGlobal - 1
 
         // Exercice : être bloquer sur la 1ère slide quand on clique sur la flêche de gauche
         // if (numArrow < 0){
@@ -43,40 +50,40 @@ $(function(){
         // } 
 
         // Bonus 1 : Infinite slider
-        if (numArrow < 0){
-            numArrow = 4
+        if (indexGlobal < 0){
+            indexGlobal = 4
         }
 
         $(".slider").animate({
-                left: `-${numArrow *100}%`
+                left: `-${indexGlobal *100}%`
         }, 1000)
+
+        clearInterval(interval)
+        interval = setInterval(autoslide, 2000)
     })
 
-    // Bonus 2 : Standalone Slider
-    var count = 0
 
-    setInterval(autoslide, 5000)
+    // Bonus 2 : Standalone Slider
+    
+    var interval = setInterval(autoslide, 5000)
     
     function autoslide() {
-        count = count - 1
+        indexGlobal = indexGlobal + 1
         
-        if (count === -5) {
-            count = 0
-        }
-        if (count === 1) {
-            count = -4
+        if (indexGlobal > 4){
+            indexGlobal = 0
         }
 
         $(".slider").animate({
-            left: `${count*100}%`
-        })
+            left: `-${indexGlobal*100}%`
+        },1000)
     }
 
+
     // Bonus 3 : Random pictures
-    var random = 0
-    for (var i = 1; i <= 5; i++){
-        random = Math.random()
+    $(".slide").each(function(i){
+        var random = Math.random()
         $(`.slide:nth-child(${i})`).css("background-image", `url("https://picsum.photos/1600/900?${random}")`)
-    }
+    })
 
 })
